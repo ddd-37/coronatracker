5b94d43176f97c9e0c5532dccb3fa6f9ee0ed377
Devon Deason <devon.d.deason@gmail.com>
2020-05-05 17:14:05 -0600

    set up our api request

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Helmet from "react-helmet";
 
+import axios from "axios";
+
 import Layout from "components/Layout";
 import Container from "components/Container";
 import Map from "components/Map";
@@ -19,7 +21,19 @@ const IndexPage = () => {
    * @example Here this is and example of being used to zoom in and set a popup on load
    */
 
-  async function mapEffect({ leafletElement: map } = {}) {}
+  async function mapEffect({ leafletElement: map }) {
+    let response;
+
+    try {
+      response = await axios.get("https://corona.lmao.ninja/v2/countries");
+    } catch (e) {
+      console.log(`Failed to fetch countries: ${e.message}`, e);
+      return;
+    }
+
+    const { data = [] } = response;
+    console.log("mapEffect -> data", data);
+  }
 
   const mapSettings = {
     center: CENTER,
