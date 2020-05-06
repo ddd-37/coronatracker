5fdc0ef9923da801e45f65f5e09db9c71e33bb02
Devon Deason <devon.d.deason@gmail.com>
2020-05-06 08:43:13 -0600

    get our data and display it

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Helmet from "react-helmet";
+import L from "leaflet";
 
 import axios from "axios";
 
@@ -33,6 +34,77 @@ const IndexPage = () => {
 
     const { data = [] } = response;
     console.log("mapEffect -> data", data);
+
+    // Check to see if our data is an array and had data
+    const hasData = Array.isArray(data) && data.length > 0;
+
+    if (!hasData) return;
+
+    // Create our GeoJSON document
+    const geoJson = {
+      type: "FeatureCollection",
+      features: data.map((country = {}) => {
+        const { countryInfo = {} } = country;
+        const { lat, long: lng } = countryInfo;
+        return {
+          type: "Feature",
+          properties: {
+            ...country,
+          },
+          geometry: {
+            type: "Point",
+            coordinates: [lng, lat],
+          },
+        };
+      }),
+    };
+
+    // Create a new instance of L.GeoJSON which will transform our GeoJSON document into something Leaflet will understand
+    const geoJsonLayers = new L.GeoJSON(geoJson, {
+      // Define a custom pointToLayer function. This allows us to customize the map layer Leaflet creates for our map
+      pointToLayer: (feature = {}, latlng) => {
+        const { properties = {} } = feature;
+        let updatedFormatted;
+        let casesString;
+
+        const { country, updated, cases, deaths, recovered } = properties;
+
+        casesString = `${cases}`;
+
+        if (cases > 1000) {
+          casesString = `${casesString.slice(0, -3)}k+`;
+        }
+
+        if (updated) {
+          updatedFormatted = new Date(updated).toLocaleString();
+        }
+
+        // Tooltip HTML
+        const html = `
+          <span class="icon-marker">
+            <span class="icon-marker-tooltip">
+              <h2>${country}</h2>
+              <ul>
+              <li><strong>Confirmed:</strong> ${cases}</li></ul>
+              <li><strong>Deaths:</strong> ${deaths}</li></ul>
+              <li><strong>Recovered:</strong> ${recovered}</li></ul>
+              <li><strong>Last Update:</strong> ${updatedFormatted}</li></ul>
+            </span>
+            ${casesString}
+          </span>
+        `;
+
+        return L.marker(latlng, {
+          icon: L.divIcon({
+            className: "icon",
+            html,
+          }),
+          riseOneHover: true,
+        });
+      },
+    });
+
+    geoJsonLayers.addTo(map);
   }
 
   const mapSettings = {
