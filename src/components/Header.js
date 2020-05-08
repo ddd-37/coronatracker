636f0ba9eebb5218530b1b614ab7c016b85781cb
Devon Deason <devon.d.deason@gmail.com>
2020-05-08 10:02:39 -0600

    rmove second page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Link } from 'gatsby';
+import React from "react";
+import { Link } from "gatsby";
 
-import Container from 'components/Container';
+import Container from "components/Container";
 
 const Header = () => {
   return (
@@ -11,9 +11,6 @@ const Header = () => {
         <ul>
           <li>
             <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2/">Page 2</Link>
           </li>
         </ul>
       </Container>
