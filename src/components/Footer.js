9034d67a4e53f3f54d3a9a913be671262bb02c21
Devon Deason <devon.d.deason@gmail.com>
2020-05-09 09:25:01 -0600

    styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,10 @@ const Footer = () => {
   return (
     <footer>
       <p>&copy; {new Date().getFullYear()}, Devon D Deason</p>
+      <p>
+        Data provided from the fine folks at{" "}
+        <a href="https://corona.lmao.ninja/">corona.lmao.ninja</a>
+      </p>
     </footer>
   );
 };
