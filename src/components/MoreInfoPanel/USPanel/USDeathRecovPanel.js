4fb299ac74ad833e18aa15daef41296647b2c508
Devon Deason <devon.d.deason@gmail.com>
2020-05-09 08:23:54 -0600

    recovered panel

--- src/components/MoreInfoPanel/USPanel/USDeathRecovPanel.js
+++ src/components/MoreInfoPanel/USPanel/USDeathRecovPanel.js
@@ -6,7 +6,37 @@ const USDeathRecovPanel = () => {
   const data = useContext(Data).states;
   console.log("USDeathRecovPanel -> data", data);
 
-  return <div></div>;
+  return (
+    <div>
+      <h3>US State level</h3>
+      <h4>
+        <strong>
+          <span style={{ color: "red" }}>Deaths</span> /
+          <span style={{ color: "green" }}> Recovered</span>
+        </strong>
+      </h4>
+      <div>
+        {data.map((state, i) => {
+          const recovered = state.cases - state.deaths - state.active;
+          return (
+            <div key={state + i}>
+              <p>
+                <span style={{ color: "red" }}>
+                  {state.deaths.toLocaleString()}
+                </span>{" "}
+                /
+                <span style={{ color: "green" }}>
+                  {" "}
+                  <strong>{recovered.toLocaleString()}</strong>
+                </span>
+              </p>
+              <p>{state.state}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default USDeathRecovPanel;
