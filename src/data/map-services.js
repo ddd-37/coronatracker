e231857a466746a885a3099127ba6404a6e3b19f
Devon Deason <devon.d.deason@gmail.com>
2020-05-10 17:39:58 -0600

    set token as var

--- src/data/map-services.js
+++ src/data/map-services.js
@@ -1,3 +1,5 @@
+const mapboxToken = process.env.GATSBY_MAPBOX_TOKEN;
+
 export const mapServices = [
   {
     name: "OpenStreetMap",
@@ -9,6 +11,6 @@ export const mapServices = [
     name: "Mapbox",
     attribution:
       "Map data &copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors, <a href=&quot;https://creativecommons.org/licenses/by-sa/2.0/&quot;>CC-BY-SA</a>, Imagery &copy; <a href=&quot;https://www.mapbox.com/&quot;>Mapbox</a>",
-    url: `https://api.mapbox.com/styles/v1/ddeason37/ck9vhil4301ij1iqvye0i4497/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.GATSBY_APP_MAPBOX_TOKEN}`,
+    url: `https://api.mapbox.com/styles/v1/ddeason37/ck9vhil4301ij1iqvye0i4497/tiles/256/{z}/{x}/{y}@2x?access_token=${mapboxToken}`,
   },
 ];
